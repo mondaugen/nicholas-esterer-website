acded9aa0679347dfbf1665b0bedf87ca05b063d
Nicholas Esterer <nicholas.esterer@gmail.com>
2020-04-03 12:11:35 -0400

    Should now always plot waterlevel comparisons

--- update_waterlevels.sh
+++ update_waterlevels.sh
@@ -1,3 +1,4 @@
 export PATH="$(dirname $0)/../../virtualenv/dummyapp/3.7/bin/:$PATH"
-DB_PATH="$(dirname $0)/waterlevels/data/waterlevel_history" bash "$(dirname $0)/waterlevels/update_waterlevels.sh"
+STATION_ID=15520 DB_PATH="$(dirname $0)/waterlevels/data/waterlevel_history_15520" bash "$(dirname $0)/waterlevels/update_waterlevels.sh"
+STATION_ID=15540 DB_PATH="$(dirname $0)/waterlevels/data/waterlevel_history_15540" bash "$(dirname $0)/waterlevels/update_waterlevels.sh"
 
